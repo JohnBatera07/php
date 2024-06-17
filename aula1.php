691578c1aac83dd6fc140cb4a0f18d4257508c93
jv07cg85 <joaovithorbragagomes@gmail.com>
2024-06-17 20:16:00 -0300

    Aula 1 PHP

---
+++ aula1.php
@@ -0,0 +1,12 @@
+<?php
+    //echo "Aula 1";
+    $nome = "Felipe";
+    $idade = 20;
+    $altura = 1.70;
+    $logico = true;
+    const senac = "Caxias";
+
+
+    echo $nome . "<br>" . $idade . "<br>" . $altura . "<br>" . $logico . "<br>";
+    echo senac;
+?>
