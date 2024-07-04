190311b2d6b6dc50a843c3484835eec0eb45d045
jv07cg85 <joaovithorbragagomes@gmail.com>
2024-07-03 21:42:43 -0300

    Aulas 1, 2, 3, 4, 5, 6, 7, 8 e 9 PHP

---
+++ aula9/sistema/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jvbat/sistema',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fcb9c3b9d193797712eb238fe0e01f7f3fec70d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jvbat/sistema' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fcb9c3b9d193797712eb238fe0e01f7f3fec70d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
